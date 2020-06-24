d2c72ef4f2f574085b415177e87ea5a604f5c159
sohan-shingade <sohanshingade@gmail.com>
2020-06-24 14:00:55 -0700

    Updates

---
+++ precache-manifest.769d2e18d19aad11db7a2e57939c4177.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372cbc12602ab9fba8b6440dee695579",
+    "url": "/DAOTutorsWebsite/index.html"
+  },
+  {
+    "revision": "9eb565bd0f47df74c29d",
+    "url": "/DAOTutorsWebsite/static/js/2.73501c13.chunk.js"
+  },
+  {
+    "revision": "cd6b660f9b77f06aa09592c7b60ec6d1",
+    "url": "/DAOTutorsWebsite/static/js/2.73501c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f317b86dbcb8e3b648f7",
+    "url": "/DAOTutorsWebsite/static/js/main.0be5ed90.chunk.js"
+  },
+  {
+    "revision": "ae8a19f4e278eaf30ea7",
+    "url": "/DAOTutorsWebsite/static/js/runtime-main.57437d81.js"
+  },
+  {
+    "revision": "960b99b0020beea6fb9ba3027989288e",
+    "url": "/DAOTutorsWebsite/static/media/DAOtutorlogotest3.960b99b0.png"
+  },
+  {
+    "revision": "ba2946d2e65eee3b83dffa926f9b953a",
+    "url": "/DAOTutorsWebsite/static/media/bx-user-circle.ba2946d2.svg"
+  },
+  {
+    "revision": "aac0b32fa6bb67a48b4beaeea88118d4",
+    "url": "/DAOTutorsWebsite/static/media/example-scene-1.aac0b32f.svg"
+  },
+  {
+    "revision": "b1797edfa3d02fac334f0cf30d319279",
+    "url": "/DAOTutorsWebsite/static/media/scene-one.b1797edf.png"
+  }
+]);
